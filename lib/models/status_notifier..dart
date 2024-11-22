172ffd3c78e3269f99234365b292cfd19cdb8ba2
Mansi Patil <50865213+mansi2804@users.noreply.github.com>
2024-11-21 18:56:57 -0600

    Update status_notifier..dart

--- lib/models/status_notifier..dart
+++ lib/models/status_notifier..dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'game_state.dart';
 import '../utils/session_controller.dart';
-
+ 
 class GameListNotifier extends ChangeNotifier {
   List<Game> games = [];
   bool isLoading = true;
